4d9ef89dc7fe169853d26511992b153f78def349
barton-w <bartonw@gmail.com>
2019-08-13 21:07:30 -0600

    Added Active as a string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_030116) do
+ActiveRecord::Schema.define(version: 2019_08_14_030407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_08_14_030116) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "active"
   end
 
 end
